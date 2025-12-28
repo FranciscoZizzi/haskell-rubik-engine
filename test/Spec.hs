430da3df5ae882406336e796a7dc4e8d7b4f8ffc
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2025-12-28 17:34:05 -0300

    more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,7 @@
 import Test.Hspec
 import Control.Monad
+import qualified Data.Map as Map
+
 import Cube.Data
 import Cube.DefaultCubes
 
@@ -18,6 +20,8 @@ spec = do
       executeMovements ["U","U","U","U"] defaultCube `shouldBe` Right defaultCube
     it "should result in solved cube if sequence of order 6 is repeated 6 times" $ do
       executeMovements cyclicMovement defaultCube `shouldBe` Right defaultCube
+    it "should result in error message if movement is not defined for cube" $ do
+      executeMovement defaultCube "P" `shouldBe` Left "Invalid movement: P"
   describe "isSolved" $ do
     it "should return true for a new cube" $ do
       isSolved defaultCube `shouldBe` True
@@ -25,6 +29,9 @@ spec = do
       fmap isSolved (executeMovements ["F", "U", "U", "F"] defaultCube) `shouldBe` Right False
     it "should return true for cube with movements applied but in solved state" $ do
       fmap isSolved (executeMovements cyclicMovement defaultCube) `shouldBe` Right True
+  describe "cube creation" $ do
+    it "should get error message when creating cube with invalid movements" $ do
+      newCube (Map.fromList [((0,0,0), [Sticker 0 U 0])]) (Map.fromList [("U", [((0,0,0), (1,1,1), [(U, D)], [(U, 1)])])]) True `shouldBe` Left "Invalid Movements"
 
 executeMovements :: [String] -> Cube -> Either String Cube
 executeMovements movements cube = foldM executeMovement cube movements
