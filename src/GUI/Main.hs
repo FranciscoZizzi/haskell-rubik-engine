b89751cf6fbfa32088977779b4baf85ae854c42d
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2025-12-31 19:35:14 -0300

    refactor

--- src/GUI/Main.hs
+++ src/GUI/Main.hs
@@ -3,67 +3,16 @@
 
 module GUI.Main (main) where
 
+import GUI.Widgets.CubeView (cubeView)
 import GUI.Types
-import qualified Data.Map as Map
-import qualified Data.List as List
-import Data.Maybe
-import Data.Either
-import Monomer
-import Data.Text (pack)
 
 import Cube.Data
 import Cube.DefaultCubes (defaultCube)
 
-sticker :: (WidgetModel s, WidgetEvent e) => Color -> WidgetNode s e
-sticker color = box (box (label "")
-  `styleBasic` [
-    width 45,
-    height 45,
-    bgColor color
-  ]) `styleBasic` [padding 2.5]
-
-cubeFace :: (WidgetModel s, WidgetEvent e) => [[Color]] -> WidgetNode s e
-cubeFace colors = box (
-  vgrid stickerGrids)
-  where
-    stickerGrids = map (hgrid . map sticker) colors
-
-cubeView :: (WidgetModel s, WidgetEvent e) => Cube -> Map.Map Int Color -> WidgetNode s e
-cubeView c colorMap_ = box (
-  hgrid [
-      vgrid [cubeFace lFace],
-      vgrid [
-        cubeFace uFace,
-        cubeFace fFace,
-        cubeFace dFace
-        ],
-      vgrid [cubeFace rFace],
-      vgrid [cubeFace bFace]
-    ]
-  )
-  where
-    fFace = getFaceColors F
-    uFace = getFaceColors U
-    dFace = getFaceColors D
-    lFace = getFaceColors L
-    rFace = getFaceColors R
-    bFace = getFaceColors B
-    getFaceColors face = [[getStickerColor $ getFaceSticker face $ getPiece (x,y) face | x <- [0..faceSize]] | y <- [0..faceSize]]
-    faceSize = getFaceSize c
-    getPiece coords face = pieces c Map.! transform2dto3d coords face faceSize
-    getFaceSticker face = List.find (\s -> face == orientation s)
-    getStickerColor maybeSticker = case maybeSticker of
-      Nothing -> black 
-      Just s -> fromMaybe black (Map.lookup (stickerId s) colorMap_)
-
-transform2dto3d :: (Int, Int) -> Face -> Int -> (Int, Int, Int)
-transform2dto3d (x, y) face faceSize = case face of
-  L -> (0, faceSize-x, faceSize-y)
-  U -> (x, faceSize-y, 2)
-  F -> (x, 0, faceSize-y)
-  D -> (x, y, 0)
-  R -> (2, x, faceSize-y)
-  B -> (faceSize-x, 2, faceSize-y)
+import qualified Data.Map as Map
+import Data.Either
+import Monomer
+import Data.Text (pack)
 
 getMovementButtons :: Cube -> WidgetNode AppModel AppEvent
 getMovementButtons c = vstack buttons
